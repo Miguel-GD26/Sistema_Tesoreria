eaece2bb7afbf5d5b034defd1a1ee76e61c37bab
Miguel-GD26 <henrygarcia2616@gmail.com>
2025-10-07 23:08:08 -0500

    primer commit

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const path = require('path');
 const app = express();
 
 // Define el nombre de la carpeta de distribución de Angular
-const appName = 'sistema-tesoreria'; // Asegúrate que coincida con el nombre de tu proyecto
+const appName = 'sistema-tesoreria'; // Asegúrate que coincida
 
 // Servir los archivos estáticos desde la carpeta de Angular compilada
 app.use(express.static(path.join(__dirname, `dist/${appName}/browser`)));
 
-// Para cualquier otra ruta, servir el archivo index.html (¡esto es clave para que las rutas de Angular funcionen!)
-app.get('/*', (req, res) => {
+// CORRECCIÓN AQUÍ: Para cualquier otra ruta, servir el archivo index.html
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, `dist/${appName}/browser/index.html`));
 });
 
